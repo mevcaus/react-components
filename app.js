b3ec00b13ae810c5dc5ea84f82138733795c05a9
Mevludin Causevic <mevludincausevic@gmail.com>
2023-02-21 12:36:28 -0700

    added functionality for grocerylist with bold hovering

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// TODO
+const App = () => (
+    <div>
+        <h2>My Grocery List</h2>
+        <GroceryList items={['apples', 'bananas']}/>
+    </div>
+);
+
+function GroceryList(props) {
+    return (
+        <ul>
+            {props.items.map(item => <GroceryListItem item={item} />)}
+        </ul>
+    )
+}
+
+const GroceryListItem = (prop) => {
+    const [isDone, setIsDone] = React.useState(false);
+    const style = {
+        fontWeight: isDone ? 'bold' : 'none'
+    }
+    return (
+        <li style={style}
+        onMouseEnter={() => setIsDone(true)}
+        >
+            {prop.item}
+        </li>
+    )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
